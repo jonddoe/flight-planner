05e238014a7f2b8fbddb78f8a5af22d5869e3556
jonddoe <79166658+jonddoe@users.noreply.github.com>
2021-06-27 20:28:33 +0300

    FlightPlanner done

---
+++ flight-planner-master/Controllers/TestingApiController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using FlightPlanner.Models;
+
+namespace FlightPlanner.Controllers
+{
+    public class TestingApiController : ApiController
+    {
+        [Route("testing-api/clear"), HttpPost]
+        public IHttpActionResult Clear()
+        {
+            FlightStorage.AllFlights.Clear();
+            AirportStorage.AllAirports.Clear();
+            return Ok();
+        }
+    }
+}
---
+++ flight-planner-master/Models/PageResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace FlightPlanner.Models
+{
+    public class PageResult
+    {
+        public int Page { get; set; }
+        public int TotalItems { get; set; }
+        public List<Flight> Items = new List<Flight>();
+    }
+}
